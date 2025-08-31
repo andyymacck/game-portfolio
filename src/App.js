2accd8add626458e906093c7cb82b1680810c62e
Andrew Mackay <andyymackay@gmail.com>
2025-08-31 11:13:31 -0300

    Initial Commit
    Original styling and layout Idea

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen">
-        <Header />
-        <Routes>
-          <Route path="/" element={
-            <div>
-              <Projects />
-              <Contact />
-            </div>
-          } />
-        </Routes>
-      </div>
-    </Router>
+    <div className="portfolio-container">
+      <header>
+        <h1>Andy Mackay</h1>
+        <p>Game Developer Portfolio</p>
+      </header>
+      <section className="showcase">
+        <h2>Vertical Slices</h2>
+        {/* Add your vertical slice components or video embeds here */}
+        <div className="slice">
+          <h3>Slice Title</h3>
+          <video controls src="your-video.mp4" />
+          <p>Description of this slice...</p>
+        </div>
+        {/* Repeat for more slices */}
+      </section>
+    </div>
   );
 }
 
